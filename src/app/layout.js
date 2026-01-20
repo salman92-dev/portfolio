7486ec086bf6f46b32bae5d8f60f6c1b75657dc2
salman92-dev <sa0587676@gmail.com>
2026-01-20 20:52:10 +0500

    adding schema to website

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,11 +21,45 @@ export const metadata = {
     "web developer",
     "react developer",
   ],
+   icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Salman's Portfolio",
+    description: "Welcome to Salman’s professional portfolio showcasing expertise in web development, design, and innovative projects.",
+    url: "https://salmanlabs.online",
+    siteName: "Your Brand Name",
+    images: [
+      {
+        url: "https://salmanlabs.online/logo.png",
+        width: 300,
+        height: 300,
+        alt: "Salman's Portfolio",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+          <Script
+          id="org-schema"
+          type="application/ld+json"
+          strategy="beforeInteractive"
+        >
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            name: "Salman's Portfolio",
+            url: "https://salmanlabs.online",
+            logo: "https://salmanlabs.online/logo.png",
+          })}
+        </Script>
+
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
