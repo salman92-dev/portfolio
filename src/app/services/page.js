ed77badb4790c01d146d57204edba6b59b6af147
salman92-dev <sa0587676@gmail.com>
2026-01-22 18:40:33 +0500

    changing stack order of alert

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -10,7 +10,7 @@ const Services = () => {
     <div className="overflow-hidden">
         <Navbar/>
         <Main />
-        <h2 className="fixed z-[99] top-[30%] left-[2%] md:left-[20%] text-[6vw] font-bold text-center funnel max-w-4xl mx-auto p-6 border-5 border-[#000] bg-black text-white rounded-4xl">This page is In Development</h2>
+        <h2 className="fixed z-[40] top-[30%] left-[2%] md:left-[20%] text-[6vw] font-bold text-center funnel max-w-4xl mx-auto p-6 border-5 border-[#000] bg-black text-white rounded-4xl">This page is In Development</h2>
         <Footer/>
     </div>
   );
